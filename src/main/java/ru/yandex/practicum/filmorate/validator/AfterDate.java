b703d939922e126413a50d5febc5421f20fe3262
LeonidBS <l.kim4p@yandex.ru>
2023-03-25 00:45:52 +0300

    Выполнение ТЗ9 (ветка controllers-films-user), первая итерация, удаление лишних файлов из PR

---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/AfterDate.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = DateValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AfterDate {
+    String message() default "Не корректная дата";
+    String date();
+
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        AfterDate[] value();
+    }
+}
+
