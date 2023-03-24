54e4157f2679efb38f8e4ed5f211ac95026c8cfb
LeonidBS <l.kim4p@yandex.ru>
2023-03-25 00:45:52 +0300

    Выполнение ТЗ9 (ветка controllers-films-user), первая итерация

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationExcpretion.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class ValidationExcpretion extends RuntimeException {
+    public ValidationExcpretion(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/DurationIsPositive.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = DurationValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DurationIsPositive {
+    String message() default "Не корректная продолжительность";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
