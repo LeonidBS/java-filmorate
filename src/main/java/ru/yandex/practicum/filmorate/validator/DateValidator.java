64e315db2e9666169deac76cdd399a79344c85d8
LeonidBS <l.kim4p@yandex.ru>
2023-03-25 03:20:51 +0300

    ТЗ№9, первая итерация

---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/DateValidator.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class DateValidator implements ConstraintValidator<AfterDate, LocalDate> {
+    private LocalDate startDate;
+
+    @Override
+    public void initialize(AfterDate afterDateAnnotation) {
+        startDate = LocalDate.parse(afterDateAnnotation.date());
+    }
+
+    @Override
+    public boolean isValid(LocalDate filmReleaseDate,
+                           ConstraintValidatorContext cxt) {
+        return filmReleaseDate.isAfter(startDate);
+    }
+
+}
