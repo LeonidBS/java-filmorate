d974882921a9aa0a264ee01102b745c40d68b80c
LeonidBS <l.kim4p@yandex.ru>
2023-05-09 15:50:09 +0300

    Sprint#14/11, feat, refactor, test. Выполнение ТЗ№11 задания. Итерация 1.

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -1,17 +1,20 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.exception.IdNotFoundException;
 import ru.yandex.practicum.filmorate.exception.IdPassingException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Friends;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
+@Qualifier("mem")
 public class InMemoryUserStorage implements UserStorage {
     private final Map<Integer, User> users = new HashMap<>();
     private int id = 1;
@@ -24,41 +27,20 @@ public class InMemoryUserStorage implements UserStorage {
 
     @Override
     public User findById(Integer id) {
-        User user = users.get(id);
 
-        if (user == null) {
-            log.error("Пользователь с переданным ID {} не существует", id);
-            throw new IdNotFoundException("Не существует пользвателя с ID: " + id);
-        }
-
-        return user;
+        return users.get(id);
     }
 
     public User create(User user) {
-
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-
         user.setId(id++);
         users.put(user.getId(), user);
-        log.debug("Добавен пользователь: {}", user);
+
         return user;
     }
 
     public User update(User user) {
-
-        if (!users.containsKey(user.getId())) {
-            log.error("Пользователь с переданным ID {} не существует", user.getId());
-            throw new IdNotFoundException("Не существует пользвателя с ID " + user.getId());
-        }
-
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-
-        log.debug("Обновлен пользователь: {}", user);
         users.put(user.getId(), user);
+
         return user;
     }
 
@@ -78,17 +60,53 @@ public class InMemoryUserStorage implements UserStorage {
     public Friends deleteFriends(Integer invitor, Integer invitee) {
         Friends friends = new Friends(invitor, invitee);
         if (allFriends.remove(friends) != null) {
-            log.debug("Пользователи с ID {}, {} удалены из друзей: ", invitor,invitee);
+            log.debug("Пользователи с ID {}, {} удалены из друзей: ", invitor, invitee);
             return friends;
         } else {
-            log.error("Друзей с ID {}, {} не существует", invitor,invitee);
+            log.error("Друзей с ID {}, {} не существует", invitor, invitee);
             throw new IdPassingException("Не существует  друзей с ID :" +
                     invitor + ", " + invitee);
         }
     }
 
-    @Override
-    public Collection<Friends> findAllFriends() {
-        return allFriends.keySet();
+    public List<User> findFriendsById(Integer id) {
+
+        return allFriends.keySet().stream()
+                .filter(friends -> friends.getInviter().equals(id)
+                        | friends.getInvitee().equals(id))
+                .map(friends -> {
+                    if (friends.getInviter().equals(id)) {
+                        return findById(friends.getInvitee());
+                    } else {
+                        return findById(friends.getInviter());
+                    }
+                })
+                .sorted(Comparator.comparingInt(User::getId))
+                .collect(Collectors.toList());
+    }
+
+    public List<User> findMutualFriendsByTwoIds(Integer id, Integer friendId) {
+        Friends friends = new Friends(id, friendId);
+
+        return allFriends.keySet().stream()
+                .filter(f -> (f.getInviter().equals(id) || f.getInvitee().equals(id))
+                        || (f.getInviter().equals(friendId) || f.getInvitee().equals(friendId)))
+                .filter(f -> !f.equals(friends))
+                .map(f -> {
+                    if (f.getInviter().equals(id) || f.getInviter().equals(friendId)) {
+                        return findById(f.getInvitee());
+                    } else {
+                        return findById(f.getInviter());
+                    }
+                })
+                .collect(Collectors.toList())
+                .stream()
+                .collect(Collectors.groupingBy(Function.identity()))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue().size() == 2)
+                .map(Map.Entry::getKey)
+                .sorted(Comparator.comparingInt(User::getId))
+                .collect(Collectors.toList());
     }
 }
